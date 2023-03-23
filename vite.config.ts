478c78e37c966c6eef552b5026c85c465c549687
Henry YP Ho <me@h3nr7.com>
2023-03-23 10:32:52 +0000

    added declaration file build via vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import glsl from 'vite-plugin-glsl'
+import dts from "vite-plugin-dts"
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
 export default defineConfig({
-  plugins: [glsl()],
+  plugins: [
+    glsl(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
